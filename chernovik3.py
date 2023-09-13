b4ff95eed6afd20b3c1c5fa72ec9bb1021d2e0e4
alena <alenka200124@gmail.com>
2023-09-13 12:20:31 +0300

    sort.py vers 1.4 chrnovik3

--- chernovik3.py
+++ chernovik3.py
@@ -7,27 +7,33 @@ def main():
     paths_from = 'C:/Users/Admin/Downloads/TRASH'
     #fun_trying(paths_from)
 
-    print(fun_IMAGE(paths_from))
+    print('\n', fun_images(paths_from), '\n')
+    print('\n', fun_documents(paths_from), '\n')
 
 
-def fun_IMAGE(paths_from):
+def fun_images(paths_from):
     
-    paths_to = os.path.join('C:/Users/Admin/Downloads/TRASH', 'IMAGE')
+    list_images = []
 
+    paths_to = os.path.join('C:/Users/Admin/Downloads/TRASH', 'images')
     if os.path.exists(paths_to) == False:
         os.mkdir(paths_to)
 
+    print('os.listdir(paths_from):\n', os.listdir(paths_from))
     for file in os.listdir(paths_from):
-        print(file)
-
-        if file != 'IMAGE' and Path(file).suffix.upper() in ('.JPEG', '.PNG', '.JPG', '.SVG'):
+        #print(file)
+        #print(Path(file).suffix.upper())
+        if file != 'images' and Path(file).suffix.upper() in ('.JPEG', '.PNG', '.JPG', '.SVG'):
+                os.rename(f'{paths_from}\{file}', normalize(f'{paths_from}\{file}'))
                 print('name of file: ', file, '- image')
                 path_join = os.path.join(paths_from, file)
                 print('path_join: ', path_join, '\n')
                 shutil.copy2(path_join, paths_to)
                 #shutil.move(path_join, paths_to)
-                return True
+                list_images.append(file)
+    return list_images
 
+    '''
         if os.path.isdir(f'{paths_from}/{file}') and file != 'IMAGE':
 
             dirContents = os.listdir(f'{paths_from}/{file}')
@@ -37,7 +43,51 @@ def fun_IMAGE(paths_from):
             else:
                 print(f'{file} not empty')
                 return fun_IMAGE(f'{paths_from}/{file}')
-                
+    '''        
+
+
+def fun_documents(paths_from):
+    
+    list_documents= []
+
+    paths_to = os.path.join('C:/Users/Admin/Downloads/TRASH', 'documents')
+    if os.path.exists(paths_to) == False:
+        os.mkdir(paths_to)
+
+    #print('os.listdir(paths_from):\n', os.listdir(paths_from))
+    for file in os.listdir(paths_from):
+        #print(file)
+        #print(Path(file).suffix.upper())
+        if file != 'documents' and Path(file).suffix.upper() in ('.DOC', '.DOCX', '.TXT', '.PDF', '.XLSX', '.PPTX'):
+                os.rename(f'{paths_from}/{file}', normalize(f'{paths_from}/{file}'))
+                #print('name of file: ', file, '- doc')
+                path_join = os.path.join(paths_from, file)
+                #print('path_join: ', path_join, '\n')
+                shutil.copy2(path_join, paths_to)
+                #shutil.move(path_join, paths_to)
+                list_documents.append(file)
+    return list_documents
+
+def normalize(string):
+    CYRILLIC_SYMBOLS = "абвгдеёжзийклмнопрстуфхцчшщъыьэюяєіїґ "
+    TRANSLATION = ("a", "b", "v", "g", "d", "e", "e", "j", "z", "i", "j", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u",
+                "f", "h", "ts", "ch", "sh", "sch", "", "y", "", "e", "yu", "ya", "je", "i", "ji", "g", "_")
+    
+    TRANS = {}
+
+    for c, l in zip(CYRILLIC_SYMBOLS, TRANSLATION): 
+        TRANS[ord(c)] = l
+        TRANS[ord(c.upper())] = l.upper()
+        
+    
+    word = ""
+    for ch in string:
+        word += ch.translate(TRANS)
+
+    print(word)
+    return word
+
+
 
 def fun_trying(paths_from):
 
@@ -52,9 +102,9 @@ def fun_trying(paths_from):
 
     for file in os.listdir(paths_from):
         if file != 'moved':
-            print('name of file: ', file)
+            #print('name of file: ', file)
             path_join = os.path.join(paths_from, file)
-            print('path_join: ', path_join, '\n')
+            #print('path_join: ', path_join, '\n')
             #shutil.copy2(path_join, paths_to)
             shutil.move(path_join, paths_to)
 
